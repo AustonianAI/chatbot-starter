9bdf97c650e4eb082ef8ef686f10f9c349b24da7
Austin Johnson <austin@austinjohnson.me>
2023-05-16 15:33:51 -0500

    ChatBot Ready

--- README.md
+++ README.md
@@ -1,21 +1,41 @@
-# 📦 Streamlit App Starter Kit 
-```
-⬆️ (Replace above with your app's name)
-```
+# ChatBot Starter with Streamlit, OpenAI, and LangChain
 
-Description of the app ...
+This repository contains a simple but powerful chatbot built with Streamlit, OpenAI, and LangChain. The chatbot maintains conversational memory, meaning it can reference past exchanges in its responses.
 
-## Demo App
+## Overview
 
-[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://starter-kit.streamlitapp.com/)
+The chatbot is a demonstration of integrating OpenAI's GPT model, LangChain library, and Streamlit for creating interactive web applications. The bot's conversational memory allows it to maintain context during the chat session, leading to a more coherent and engaging user experience.
 
-## Section Heading
+### Key Features
 
-This is filler text, please replace this with text for this section.
+- **Streamlit:** A powerful, fast Python framework used to create the web interface for the chatbot.
+- **OpenAI's GPT:** A state-of-the-art language processing AI model that generates the chatbot's responses.
+- **LangChain:** A wrapper library for the ChatGPT model that helps manage conversation history and structure the model's responses.
 
-## Further Reading
+## How to Run
 
-This is filler text, please replace this with a explanatory text about further relevant resources for this repo
-- Resource 1
-- Resource 2
-- Resource 3
+### Prerequisites
+
+- Python 3.6 or higher
+- Streamlit
+- LangChain
+- OpenAI API key
+
+### Steps
+
+1. Clone this repository.
+2. Install the necessary Python packages using the command `pip install -r requirements.txt`.
+3. Set the environment variable for your OpenAI API key.
+4. Run the Streamlit app using the command `streamlit run app.py`.
+
+## Usage
+
+The chatbot starts with a system message that sets the tone for the conversation. It then alternates between receiving user inputs and generating AI responses. The conversation history is stored and used as context for generating future responses, allowing the chatbot to maintain conversational continuity.
+
+## Contribution
+
+Contributions, issues, and feature requests are welcome. Feel free to check the [Issues](https://github.com/AustonianAI/ChatBot_Starter/issues) page if you want to contribute.
+
+## License
+
+This project is licensed under the terms of the MIT license.
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,86 +1,110 @@
+# Import required libraries
 from dotenv import load_dotenv
+from itertools import zip_longest
 
 import streamlit as st
 from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
 
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
 from langchain.schema import (
     SystemMessage,
     HumanMessage,
     AIMessage
 )
 
+# Load environment variables
 load_dotenv()
 
-st.set_page_config(page_title="Staples Chat")
+# Set streamlit page configuration
+st.set_page_config(page_title="ChatBot Starter")
 
-with st.sidebar:
-    st.title('Staples Chat')
-    add_vertical_space(2)
-    st.write(
-        'Made by [Austin Johnson](<https://github.com/AustonianTX>)')
+# Initialize session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []  # Store AI generated responses
 
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [
-        SystemMessage(
-            content="You are a pirate.  Always answer the in form of a pirate.")
-    ]
+if 'past' not in st.session_state:
+    st.session_state['past'] = []  # Store past user inputs
 
-st.write(st.session_state)
+if 'entered_prompt' not in st.session_state:
+    st.session_state['entered_prompt'] = ""  # Store the latest user input
 
-input_container = st.container()
-colored_header(label='', description='', color_name='blue-30')
-response_container = st.container()
-input_container_bottom = st.container()
-
+# Initialize the ChatOpenAI model
 chat = ChatOpenAI(
-    temperature=0.5
+    temperature=0.5,
+    model_name="gpt-3.5-turbo"
 )
 
-# User input
+
+def build_message_list():
+    """
+    Build a list of messages including system, human and AI messages.
+    """
+    # Start zipped_messages with the SystemMessage
+    zipped_messages = [SystemMessage(
+        content="You are a helpful AI assistant talking with a human. If you do not know an answer, just say 'I don't know', do not make up an answer.")]
+
+    # Zip together the past and generated messages
+    for human_msg, ai_msg in zip_longest(st.session_state['past'], st.session_state['generated']):
+        if human_msg is not None:
+            zipped_messages.append(HumanMessage(
+                content=human_msg))  # Add user messages
+        if ai_msg is not None:
+            zipped_messages.append(
+                AIMessage(content=ai_msg))  # Add AI messages
+
+    return zipped_messages
 
 
-def submit_text():
-    st.session_state.text_input = st.session_state.prompt_input
-    st.session_state.prompt_input = ""
+def generate_response():
+    """
+    Generate AI response using the ChatOpenAI model.
+    """
+    # Build the list of messages
+    zipped_messages = build_message_list()
 
-# Response output
-
-
-def generate_response(prompt):
-
-    ai_response = chat(st.session_state['messages'])
+    # Generate response using the chat model
+    ai_response = chat(zipped_messages)
 
     return ai_response.content
 
 
-with response_container:
-    if "text_input" in st.session_state:
-        prompt = st.session_state.text_input
-        st.session_state['messages'].append(
-            HumanMessage(content=prompt)
-        )
+# Define function to submit user input
+def submit():
+    # Set entered_prompt to the current value of prompt_input
+    st.session_state.entered_prompt = st.session_state.prompt_input
+    # Clear prompt_input
+    st.session_state.prompt_input = ""
 
-        for i, message_obj in enumerate(st.session_state['messages'][1:], start=1):
-            is_user_message = isinstance(message_obj, HumanMessage)
-            text = message_obj.content
 
-            message(text, is_user=is_user_message, key=str(i))
+# Create a text input for user
+st.text_input('YOU: ', key='prompt_input', on_change=submit)
 
-        response = generate_response(prompt)
 
-        st.session_state['messages'].append(
-            AIMessage(content=response))
+if st.session_state.entered_prompt != "":
+    # Get user query
+    user_query = st.session_state.entered_prompt
 
-        message(response, is_user=False, key=str(
-            len(st.session_state['messages'])))
+    # Append user query to past queries
+    st.session_state.past.append(user_query)
 
-with input_container_bottom:
-    if "text_input" not in st.session_state:
-        st.session_state["text_input"] = ""
+    # Generate response
+    output = generate_response()
 
-    st.text_input('YOU: ', key='prompt_input', on_change=submit_text)
+    # Append AI response to generated responses
+    st.session_state.generated.append(output)
+
+# Display the chat history
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        # Display AI response
+        message(st.session_state["generated"][i], key=str(i))
+        # Display user message
+        message(st.session_state['past'][i],
+                is_user=True, key=str(i) + '_user')
+
+
+
+# Add credit
+st.markdown("""
+---
+Made with 🤖 by [Austin Johnson](https://github.com/AustonianAI)""")
